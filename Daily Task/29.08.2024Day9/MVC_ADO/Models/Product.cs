d528e0a2320de473598b8a3a0e761fe1689987a2
YuvarajB2002 <yuvaraj.balu2002@gmail.com>
2024-08-29 17:30:19 +0530

    Delete Daily Task/29.08.2024Day9/MVC_ADO directory

--- Daily Task/29.08.2024Day9/MVC_ADO/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVCADO.Models
-{
-    public class Product
-    {
-        public int ProductId {  get; set; }
-        public string ProductName { get; set; }
-        public int ProductPrice {  get; set; }
-    }
-}
