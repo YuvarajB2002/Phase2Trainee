7d03bf6e814c57a49bdd89794068ae19864a8af0
YuvarajB2002 <yuvaraj.balu2002@gmail.com>
2024-09-02 19:16:34 +0530

    Add files via upload

---
+++ Daily Task/02.09.2024Day11/HotelRepoPatternAssignment/Repository/IHotel.cs
@@ -0,0 +1,14 @@
+﻿using HotelRepoPatternAssignment.Models;
+
+namespace HotelRepoPatternAssignment.Repository
+{
+    public interface IHotel
+    {
+        public IEnumerable<Hotel> GetAll();
+
+        Hotel? GetHotel(int id);
+        void AddHotel(Hotel h);
+       Hotel UpdateHotel(int id, Hotel h);
+        void DeleteHotel(Hotel h);
+    }
+}
---
+++ Daily Task/02.09.2024Day11/HotelRepoPatternAssignment/Repository/IRoom.cs
@@ -0,0 +1,14 @@
+﻿using HotelRepoPatternAssignment.Models;
+
+namespace HotelRepoPatternAssignment.Repository
+{
+    public interface IRoom
+    {
+        public IEnumerable<Room> GetAll();
+
+        Room? GetRoom(int id);
+        void AddRoom(Room r);
+       Room UpdateRoom(int id, Room r);
+        void DeleteRoom(Room r);
+    }
+}
