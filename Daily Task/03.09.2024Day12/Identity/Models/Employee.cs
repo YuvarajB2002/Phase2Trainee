b483cf8df0a3308d53287939a5bcd782da210ce0
YuvarajB2002 <yuvaraj.balu2002@gmail.com>
2024-09-03 19:15:52 +0530

    Add files via upload

---
+++ Daily Task/03.09.2024Day12/Identity/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Identity.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+        
+    }
+}
